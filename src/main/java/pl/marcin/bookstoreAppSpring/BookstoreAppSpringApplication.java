26abdad9f6e2468c039fecfd89b520475e5d344d
Marcin <gasiorek.mail@gmail.com>
2023-04-19 08:30:57 +0200

    Initial commit

---
+++ src/main/java/pl/marcin/bookstoreAppSpring/BookstoreAppSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.marcin.bookstoreAppSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreAppSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/marcin/bookstoreAppSpring/BookstoreAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcin.bookstoreAppSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreAppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
